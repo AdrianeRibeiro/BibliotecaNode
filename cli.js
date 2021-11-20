d7bc62a36fd8a705b20a770d66adf054c194e2cc
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2021-11-20 11:05:19 -0300

    usando fetch

--- cli.js
+++ cli.js
@@ -8,7 +8,7 @@ async function processaTexto(caminhoDoArquivo) {
   const resultado = await pegaArquivo(caminhoDoArquivo[2])
 
   if (caminho[3] == 'validar') {
-    console.log(chalk.yellow('links validados'), ValidaURLS(resultado))
+    console.log(chalk.yellow('links validados'), await ValidaURLS(resultado))
   } else {
     console.log(chalk.yellow('lista de links'), resultado)
   }
--- http-validacao.js
+++ http-validacao.js
@@ -1,5 +1,25 @@
-function ValidaURLS(arrayLinks) {
- return arrayLinks
+const fetch = require('node-fetch')
+//import fetch from 'node-fetch'
+
+async function checaStatus(arrayLinks) {
+  const arrayStatus = await Promise.all(arrayLinks.map(async url => {
+    const res = await fetch(url)
+    return res.status
+  }))
+
+  return arrayStatus
 }
+
+function geraArrayDeURLS(arrayLinks) {
+  return arrayLinks.map(objetoLink => Object.values(objetoLink).join())
+}
+
+async function ValidaURLS(arrayLinks) {
+  const links = geraArrayDeURLS(arrayLinks)
+  const statusLinks = await checaStatus(links)
+
+  return statusLinks
+}
+
 
 module.exports = ValidaURLS;
